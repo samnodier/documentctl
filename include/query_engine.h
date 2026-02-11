516ae68948a446d3fb5efaef49bc08a6ec31eda2
Sam Nodier <sam@samnodier.com>
2026-02-11 15:06:56 -0500

    update get_search_results definition

--- include/query_engine.h
+++ include/query_engine.h
@@ -5,8 +5,8 @@
 
 typedef struct SearchEngine search_engine_t;
 
-int *get_search_results(search_engine_t *engine, const char *word,
-                        int *found_count);
+occurrence_transfer_t *get_search_results(search_engine_t *engine,
+                                          const char *word, int *found_count);
 
 int *get_doc_ids_from_search(word_occurrence_t *list, int *out_count);
 void free_results(int *results);
