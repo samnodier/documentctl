b7a84a91b9301bd093db696509279e6fb36da5d6
Sam Nodier <sam@samnodier.com>
2026-02-11 15:07:23 -0500

    update function declarations

--- include/index_structure.h
+++ include/index_structure.h
@@ -2,6 +2,7 @@
 #define INDEX_STRUCTURE_H
 
 #include <stdbool.h>
+#include <stdio.h>
 
 #define ALPHABET_SIZE 128
 
@@ -12,10 +13,17 @@ typedef struct WordOccurence {
   struct WordOccurence *next;
 } word_occurrence_t;
 
+typedef struct {
+  int doc_id;
+  int page_num;
+  long byte_offset;
+} occurrence_transfer_t;
+
 typedef struct TrieNode {
   struct TrieNode *children[ALPHABET_SIZE];
   bool isEndOfWord;
   word_occurrence_t *occurrences;
+  int char_index;
 } trie_node_t;
 
 trie_node_t *create_node(void);
@@ -23,9 +31,14 @@ word_occurrence_t *create_occurrence(int doc_id, int page_num,
                                      long byte_offset);
 void add_occurence_to_node(trie_node_t *node, int doc_id, int page_num,
                            long byte_offset);
+void relink_occurence(trie_node_t *node, int doc_id, int page_num,
+                      long byte_offset);
 void trie_insert(trie_node_t *root, const char *word, int doc_id, int page_num,
                  long byte_offset);
 word_occurrence_t *trie_search(trie_node_t *root, const char *word);
 void trie_free(trie_node_t *node);
+int trie_children_count(trie_node_t *node);
+int trie_node_serialize(trie_node_t *node, int char_index, FILE *fp);
+trie_node_t *trie_node_deserialize(FILE *fp);
 
 #endif // !INDEX_STRUCTURE_H
